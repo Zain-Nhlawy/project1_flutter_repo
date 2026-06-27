26041f8ccd38633305c817c389b2e4fdb80f4377
Zain <zainnhlawy@gmail.com>
2026-06-27 13:03:01 +0300

    pull

--- lib/features/demo/data/models/demo_model.dart
+++ lib/features/demo/data/models/demo_model.dart
@@ -2,20 +2,18 @@ import 'package:project1/features/demo/domain/entities/demo_entity.dart';
 
 class DemoModel extends DemoEntity {
   DemoModel({
-     super.id, 
+
     required super.name,
   });
 
   factory DemoModel.fromJson(Map<String, dynamic> json) {
     return DemoModel(
-      id: json['id'] as String?,
       name: json['name'] as String,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
       'name': name,
     };
   }
